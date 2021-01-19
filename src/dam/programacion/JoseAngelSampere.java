df99cded6d1e103294c40e111e7c69705f15bfbb
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-01-19 11:01:22 +0100

    ver nombre colaborador

---
+++ src/dam/programacion/JoseAngelSampere.java
@@ -0,0 +1,11 @@
+package dam.programacion;
+
+/**
+ * Creado por @autor: angel
+ * El  19 de ene. de 2021.
+ **/
+public class JoseAngelSampere {
+    public void verNombre(){
+        System.out.println("Jose Ángel Sampere Vázquez");
+    }
+}
